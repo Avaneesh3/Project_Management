009f88af21f47a00f2e539d03a87f4d15f78df9d
Avaneesh3 <82702727+Avaneesh3@users.noreply.github.com>
2022-09-18 18:17:28 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,41 @@
+class tasks:
+	def __init__():
+		datetime time_to_complete  #deadline_by_schedule
+		list users
+		list resources
+        list dependencies
+	
+	def schedule(start_date):
+		return date_time      #date of completion given constraints
+            
+class utilities:
+    def __init__():
+        bool status           #available or task assigned
+        datetime availability
+        str util_type         #user/resource
+        str util_subtype      #user/resource subtype
+        
+    def count_available_utils(int count):
+        return date_when_count_available
+    
+class schedule:
+    def __init__():
+        dict task_deadlines        #task_deadlines[task_name] = deadline
+        list dependencies
+        
+    def sort_deadlines(task_deadlines):
+        sorted_deadlines = sorted(list(task_deadlines.values()))
+        tasks_sorted_by_deadlines = {}
+        for i in range(task_deadlines)
+        return tasks_sorted_by_deadlines
+        
+    def topsortlist(AMat):
+            
+                
+    
+    
+    
+        
+        
+        
+def project_allocate(end_date,project):
