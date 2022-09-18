84ba012dfa6fb043df2d4df6ebc798fdb64c7209
Avaneesh3 <82702727+Avaneesh3@users.noreply.github.com>
2022-09-18 18:21:40 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,35 +1,39 @@
+import numpy as np
 class tasks:
-	def __init__():
-		datetime time_to_complete  #deadline_by_schedule
-		list users
-		list resources
-        list dependencies
-	
-	def schedule(start_date):
-		return date_time      #date of completion given constraints
+    def __init__(self,deadline,users,resources):
+        self.deadline = deadline
+        self.users = users
+        self.resources = resources
+
+    def schedule(start_date = 0):
+        return task_completion_date 
             
 class utilities:
-    def __init__():
-        bool status           #available or task assigned
-        datetime availability
-        str util_type         #user/resource
-        str util_subtype      #user/resource subtype
+    def __init__(self,status,available_in_days,util_type,util_subtype):
+        self.status = status
+        self.available_in_days = available_in_days
+        self.util_type = util_type
+        self.util_subtype = util_subtype
         
-    def count_available_utils(int count):
+    def count_available_utils(util_type,util_subtype,required_count):
         return date_when_count_available
     
+    
 class schedule:
-    def __init__():
-        dict task_deadlines        #task_deadlines[task_name] = deadline
-        list dependencies
+    def __init__(self,task_deadlines,dependencies):
+        self.task_deadlines = task_deadlines      #task_deadlines[task_name] = deadline
+        self.dependencies = dependencies
         
-    def sort_deadlines(task_deadlines):
+    def sort_deadlines(task_deadlines):                    #tasks_sorted_by_deadlines[task_name] = index of eadline in sorted_deadlines
         sorted_deadlines = sorted(list(task_deadlines.values()))
         tasks_sorted_by_deadlines = {}
-        for i in range(task_deadlines)
+        for i in range(task_deadlines.keys()):
+            tasks_sorted_by_deadlines[i] = sorted_deadlines.index(task_deadlines[i])
         return tasks_sorted_by_deadlines
         
     def topsortlist(AMat):
+	return topsortlist
+           
             
                 
     
@@ -39,3 +43,7 @@ class schedule:
         
         
 def project_allocate(end_date,project):
+	if j.schedule() < j.deadline:
+                
+            else:
+                return "fail"
